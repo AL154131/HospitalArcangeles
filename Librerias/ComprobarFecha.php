b18409f6f31ef6431240b55f53279fbad4040b1f
MartinVera154007 <43767778+MartinVera154007@users.noreply.github.com>
2019-04-30 17:23:59 -0600

    Correcion al formularo de citas, comprobacion de fechas pasadas

--- Librerias/ComprobarFecha.php
+++ Librerias/ComprobarFecha.php
@@ -3,6 +3,8 @@
 	$nombreMedico = $_POST['medico'];
 	$fecha = $_POST['fecha'];
 
+	$fechaActual = date('y-m-d');
+
 	// Comprobar que la fecha este compuesta de solo números //
 	if (preg_match("/Día/", $fecha) or preg_match("/Mes/", $fecha) or preg_match("/Año/", $fecha)) {
     	echo "Seleccione una fecha válida";
@@ -17,14 +19,16 @@
 		$resultado = $mysqli -> query($consultaDisponibilidad);
 		$numeroFilas = $resultado -> num_rows;
 
-		if($numeroFilas == 0) {
+		if($numeroFilas == 0 && strtotime($fecha) >= strtotime($fechaActual)) {
 			echo "
-			<p id='fechaBien'>La fecha y hora SI estan disponibles!!!</p>
+			<p id='fechaBien'>La fecha esta disponible.</p>
+			<script> document.getElementById('btnCita').disabled = false; </script>
 			";
 		}
 		else {
 			echo "
-			<p id='fechaMal'>La fecha y hora NO estan disponibles!!!</p>
+			<p id='fechaMal'>La fecha no está disponible.</p>
+			<script> document.getElementById('btnCita').disabled = true; </script>
 			";
 		}
 	}
