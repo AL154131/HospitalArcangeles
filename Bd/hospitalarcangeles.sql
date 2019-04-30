4908796fe1ad222755452f8c5cb5607242e795b7
MartinVera154007 <43767778+MartinVera154007@users.noreply.github.com>
2019-04-30 15:38:16 -0600

    Base de datos corregida

--- Bd/hospitalarcangeles.sql
+++ Bd/hospitalarcangeles.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 04-04-2019 a las 21:15:04
+-- Tiempo de generación: 30-04-2019 a las 21:34:29
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 7.2.14
 
@@ -37,25 +37,18 @@ CREATE TABLE IF NOT EXISTS `citas` (
   `idMedico` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `idMedico` (`idMedico`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=279 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `citas`
 --
 
 INSERT INTO `citas` (`id`, `nombrePaciente`, `telefonoPaciente`, `fecha`, `idMedico`) VALUES
-(1, 'Martin', '656766666', '2019-04-03 08:00:00', 1),
-(3, 'Martin', '2222222', '2019-04-11 08:00:00', 1),
-(4, 'nombrePaciente', 'telefono', '2019-04-03 08:00:00', 1),
-(5, 'andre', '7777777777', '2019-06-04 12:00:00', 1),
-(6, 'ejemplo1', '3948', '2020-03-02 09:00:00', 1),
-(7, '', '', '2019-01-01 08:00:00', 1),
-(8, 'Jorge Luis', '2341332', '2020-02-04 10:00:00', 1),
-(9, '', '', '2019-01-01 08:00:00', 1),
-(10, 'ejemplo2', '3948', '2019-01-01 08:00:00', 1),
-(11, 'Jose', 'Gutierrez', '2019-03-01 10:00:00', 1),
-(12, '', '', '2019-01-01 08:00:00', 1),
-(13, 'Roberto', '1212121212', '2019-03-07 08:00:00', 2);
+(274, 'Martin Vera', '6567622938', '2019-04-30 08:00:00', 1),
+(275, 'Martin Vera', '6567622938', '2019-04-30 09:00:00', 1),
+(276, 'Andre Valdez', '6562041245', '2019-01-01 08:00:00', 1),
+(277, 'Juan Lopez', '6561111111', '2019-01-01 09:00:00', 1),
+(278, 'Pedro', '6562020202', '2019-05-01 08:00:00', 2);
 
 -- --------------------------------------------------------
 
@@ -67,24 +60,29 @@ DROP TABLE IF EXISTS `medicos`;
 CREATE TABLE IF NOT EXISTS `medicos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `correo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `contraseña` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `especialidad` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
   `consultorio` varchar(5) COLLATE utf8_spanish_ci NOT NULL,
   `telefono` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `correo` (`correo`),
-  UNIQUE KEY `contraseña` (`contraseña`(30))
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  UNIQUE KEY `contraseña` (`password`(30))
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `medicos`
 --
 
-INSERT INTO `medicos` (`id`, `correo`, `contraseña`, `nombre`, `especialidad`, `consultorio`, `telefono`) VALUES
-(1, 'juan.martinez@hospitalarcangeles.com.mx', '123', 'Jun Martinez Soto', 'General', '101', '6560394874'),
+INSERT INTO `medicos` (`id`, `correo`, `password`, `nombre`, `especialidad`, `consultorio`, `telefono`) VALUES
+(1, 'juan.martinez@hospitalarcangeles.com.mx', '123', 'Juan Martinez Soto', 'General', '101', '6560394874'),
 (2, 'alan.silva@hospitalarcangeles.com.mx', '111', 'Alan Silva Andrade', 'Oftalmologo', '200', '6563445498'),
-(3, 'angel.soria@hospiatlangeles.com.mx', '321', 'Angel Ramirez Solis', 'General', '102', '6569866596');
+(3, 'angel.soria@hospitalarcangeles.com.mx', '321', 'Angel Ramirez Solis', 'General', '102', '6569866596'),
+(4, 'daniela.vargas@hospitalarcangeles.com.mx', '222', 'Daniela Vargas ', 'Ortopedista', '100', '6563340545'),
+(5, 'angela.reyes@hospitalarcangeles.com.mx', '333', 'Angela Reyes Diaz', 'Psiquiatra', '201', '6562223156'),
+(6, 'maria.lopez@hospitalarcangeles.com.mx', '444', 'Maria Lopez Lopez', 'Psiquiatra', '202', '6567685434'),
+(7, 'mario.garay@hospitalarcangeles.com.mx', '555', 'Mario Garay Hernandez', 'Psiquiatra', '203', '6563332211'),
+(8, 'paulina.herrera@hospitalarcangeles.com.mx', '666', 'Paulina Herrera Martinez', 'General', '103', '6563332229');
 
 --
 -- Restricciones para tablas volcadas
