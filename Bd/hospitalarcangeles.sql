c2662370b57098611e71588d0e35e8a3a965ddf4
MartinVera154007 <43767778+MartinVera154007@users.noreply.github.com>
2019-05-16 19:13:19 -0600

    Version final final!!!

--- Bd/hospitalarcangeles.sql
+++ Bd/hospitalarcangeles.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 16-05-2019 a las 06:44:47
--- Versión del servidor: 5.7.24
--- Versión de PHP: 7.2.14
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 16, 2019 at 11:44 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `hospitalarcangeles`
+-- Database: `hospitalarcangeles`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `beneficios`
+-- Table structure for table `beneficios`
 --
 
 DROP TABLE IF EXISTS `beneficios`;
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `beneficios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `beneficios`
+-- Dumping data for table `beneficios`
 --
 
 INSERT INTO `beneficios` (`idPlan`, `idEspecialidad`) VALUES
@@ -55,7 +55,7 @@ INSERT INTO `beneficios` (`idPlan`, `idEspecialidad`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `boletin`
+-- Table structure for table `boletin`
 --
 
 DROP TABLE IF EXISTS `boletin`;
@@ -64,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `boletin` (
   `fecha` date NOT NULL,
   `mensaje` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
--- Volcado de datos para la tabla `boletin`
+-- Dumping data for table `boletin`
 --
 
 INSERT INTO `boletin` (`id`, `fecha`, `mensaje`) VALUES
@@ -81,7 +81,7 @@ INSERT INTO `boletin` (`id`, `fecha`, `mensaje`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `citas`
+-- Table structure for table `citas`
 --
 
 DROP TABLE IF EXISTS `citas`;
@@ -93,10 +93,10 @@ CREATE TABLE IF NOT EXISTS `citas` (
   PRIMARY KEY (`id`),
   KEY `idUsuario` (`idUsuario`),
   KEY `idMedico` (`idMedico`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `citas`
+-- Dumping data for table `citas`
 --
 
 INSERT INTO `citas` (`id`, `idUsuario`, `idMedico`, `fecha`) VALUES
@@ -106,28 +106,28 @@ INSERT INTO `citas` (`id`, `idUsuario`, `idMedico`, `fecha`) VALUES
 (55, 1, 2, '2019-05-16 08:00:00'),
 (56, 2, 3, '2019-05-17 08:00:00'),
 (57, 1, 2, '2019-08-16 08:00:00'),
-(58, 0, 4, '2019-06-19 08:00:00'),
-(59, 1, 1, '2019-05-17 08:00:00');
+(59, 1, 1, '2019-05-17 08:00:00'),
+(60, 4, 8, '2020-07-04 13:00:00');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `especialidades`
+-- Table structure for table `especialidades`
 --
 
 DROP TABLE IF EXISTS `especialidades`;
 CREATE TABLE IF NOT EXISTS `especialidades` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `nombreEspecialidad` varchar(50) COLLATE utf8_spanish_ci DEFAULT NULL,
   `costoCita` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `especialidades`
+-- Dumping data for table `especialidades`
 --
 
-INSERT INTO `especialidades` (`id`, `nombre`, `costoCita`) VALUES
+INSERT INTO `especialidades` (`id`, `nombreEspecialidad`, `costoCita`) VALUES
 (1, 'General', '300.00'),
 (2, 'Oftalmologo', '500.00'),
 (3, 'Ortopedista', '1000.00'),
@@ -137,12 +137,12 @@ INSERT INTO `especialidades` (`id`, `nombre`, `costoCita`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `medicos`
+-- Table structure for table `medicos`
 --
 
 DROP TABLE IF EXISTS `medicos`;
 CREATE TABLE IF NOT EXISTS `medicos` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `correo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `password` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
@@ -152,14 +152,14 @@ CREATE TABLE IF NOT EXISTS `medicos` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `correo` (`correo`),
   KEY `fk_especialidad` (`especialidad`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `medicos`
+-- Dumping data for table `medicos`
 --
 
 INSERT INTO `medicos` (`id`, `correo`, `password`, `nombre`, `especialidad`, `consultorio`, `telefono`) VALUES
-(1, 'juan.martinez@hospitalarcangeles.com', 'hehexd', 'Juan Martinez Soto', 1, '101', '6563432122'),
+(1, 'juan.martinez@hospitalarcangeles.com', '123', 'Juan Martinez Soto', 1, '101', '6563432122'),
 (2, 'adrian.garcia@hospitalarcangeles.com', '222', 'Adrian Garcia', 2, '102', '6564433456'),
 (3, 'roberto.gomez@hospitalarcangeles.com', '333', 'Roberto Gomez', 3, '200', '6563322111'),
 (4, 'karla.estrada@hospitalarcangeles.com', '111', 'Karla Estrada Solis', 1, '100', '6567766666'),
@@ -178,7 +178,7 @@ INSERT INTO `medicos` (`id`, `correo`, `password`, `nombre`, `especialidad`, `co
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `planes`
+-- Table structure for table `planes`
 --
 
 DROP TABLE IF EXISTS `planes`;
@@ -190,19 +190,19 @@ CREATE TABLE IF NOT EXISTS `planes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `planes`
+-- Dumping data for table `planes`
 --
 
 INSERT INTO `planes` (`id`, `nombre`, `porcentajeDesc`) VALUES
 (0, 'sinPlan', '0.00'),
-(1, 'Basico', '0.15'),
-(2, 'Medio', '0.30'),
-(3, 'Completo', '0.50');
+(1, 'Basico', '0.10'),
+(2, 'Medio', '0.15'),
+(3, 'Completo', '0.30');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
 DROP TABLE IF EXISTS `usuarios`;
@@ -218,44 +218,44 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `cvv` varchar(5) COLLATE utf8_spanish_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tipoPlan` (`plan`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Volcado de datos para la tabla `usuarios`
+-- Dumping data for table `usuarios`
 --
 
 INSERT INTO `usuarios` (`id`, `correo`, `contra`, `plan`, `nombre`, `telefono`, `numTarjeta`, `fechaVencimiento`, `cvv`) VALUES
 (0, NULL, NULL, 0, 'Invitado', NULL, '', '2019-05-01', ''),
 (1, 'veramartin293@gmail.com', '123', 1, 'Martin Vera', '6563445498', '6456787654345678', '2019-05-17', '123'),
 (2, 'iandaniel@gmail.com', '111', 2, 'Ian Daniel', '6561615396', '9658741236547896', '2019-05-16', '698'),
-(3, 'carlos@gmail.com', '222', 3, 'Carlos Martinez', '6567777771', '5676543323456765', '2019-11-07', '343');
+(3, 'carlos@gmail.com', '222', 3, 'Carlos Martinez', '6567777771', '5676543323456765', '2019-11-07', '343'),
+(4, 'susanavilla44@gmail.com', '12345', 3, 'Susana Avilla', '6569874563', '8542156985632564', '2020-05-10', '965');
 
 --
--- Restricciones para tablas volcadas
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `beneficios`
+-- Constraints for table `beneficios`
 --
 ALTER TABLE `beneficios`
   ADD CONSTRAINT `beneficios_ibfk_1` FOREIGN KEY (`idPlan`) REFERENCES `planes` (`id`),
   ADD CONSTRAINT `beneficios_ibfk_2` FOREIGN KEY (`idEspecialidad`) REFERENCES `especialidades` (`id`);
 
 --
--- Filtros para la tabla `citas`
+-- Constraints for table `citas`
 --
 ALTER TABLE `citas`
-  ADD CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`),
-  ADD CONSTRAINT `citas_ibfk_3` FOREIGN KEY (`idMedico`) REFERENCES `medicos` (`id`);
+  ADD CONSTRAINT `citas_ibfk_2` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`);
 
 --
--- Filtros para la tabla `medicos`
+-- Constraints for table `medicos`
 --
 ALTER TABLE `medicos`
   ADD CONSTRAINT `fk_especialidad` FOREIGN KEY (`especialidad`) REFERENCES `especialidades` (`id`);
 
 --
--- Filtros para la tabla `usuarios`
+-- Constraints for table `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`plan`) REFERENCES `planes` (`id`);
